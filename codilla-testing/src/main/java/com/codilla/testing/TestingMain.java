36eb0ff55ac3372a9ffcbbb98d37996047da36ed
Aleksander Orlowski <alek.j.orlowski@gmail.com>
2023-01-21 19:37:03 +0100

    test

--- codilla-testing/src/main/java/com/codilla/testing/TestingMain.java
+++ codilla-testing/src/main/java/com/codilla/testing/TestingMain.java
@@ -3,5 +3,6 @@ package com.codilla.testing;
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        //
     }
 }
