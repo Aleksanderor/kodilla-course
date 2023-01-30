67a4a9e8508b8fa652faff206a26b8eb4ecf8290
Aleksander Orlowski <alek.j.orlowski@gmail.com>
2023-01-31 00:30:57 +0100

    testss

--- codilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ codilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,36 +1,28 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
 
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 7;
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
+        System.out.println("Test 1 - addAToB method:");
+        int addResult = calculator.addAToB(a, b);
+        if (addResult == 12) {
+            System.out.println("Test OK");
         } else {
             System.out.println("Error!");
         }
 
-
-    Calculator calculator = new Calculator();
-
-    int addResult = calculator.addAToB(2,2);
-    int substractResult = calculator.SubstractAFromB(9,8);
-
-       if(addResult == 4) {
-           System.out.println("Adding method in calculator works :) ");
-       } else {
-           System.out.println("Adding method in calculator class dosent work : ( ");
-       }
-       if (substractResult == 1) {
-           System.out.println("Substracting metod in calculator class works :)");
-       }else{
-           System.out.println("Substracting method in calculator class doest work : (");
-       }
+        System.out.println("Test 2 - subtractAFromB method:");
+        int subtractResult = calculator.SubstractAFromB(a, b);
+        if (subtractResult == -2) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
