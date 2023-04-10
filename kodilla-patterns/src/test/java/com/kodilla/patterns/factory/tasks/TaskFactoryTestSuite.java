9b5afc344be06cce1d5786f2aa3056c692bbc5e7
Aleksander Orlowski <alek.j.orlowski@gmail.com>
2023-04-10 20:42:35 +0200

    c

--- kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
@@ -1,44 +1,11 @@
 package com.kodilla.patterns.factory.tasks;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TaskFactoryTestSuite {
-    @Test
-    void testFactoryPutTaskInMap() {
-        // given
-        TaskFactory factory = new TaskFactory();
-        Map<Task, Boolean> tasks = new HashMap<>();
-
-        // when
-        Task task1 = factory.makeTask(TaskFactory.SHOPPING_TASK);
-        tasks.put(task1, task1.isTaskExecuted());
-
-        Task task2 = factory.makeTask(TaskFactory.PAINTING_TASK);
-        tasks.put(task2, task2.isTaskExecuted());
-
-        Task task3 = factory.makeTask(TaskFactory.DRIVING_TASK);
-        tasks.put(task3, task3.isTaskExecuted());
-
-        // then
-        Assertions.assertEquals("SHOPPING TASK", task1.getTaskName());
-        Assertions.assertEquals("PAINTING TASK", task2.getTaskName());
-        Assertions.assertEquals("DRIVING TASK", task3.getTaskName());
-
-        Assertions.assertTrue(tasks.containsKey(task1));
-        Assertions.assertTrue(tasks.containsKey(task2));
-        Assertions.assertTrue(tasks.containsKey(task3));
-
-        Assertions.assertFalse(tasks.get(task1));
-        Assertions.assertFalse(tasks.get(task2));
-        Assertions.assertFalse(tasks.get(task3));
-    }
 
     @Test
     void testFactoryPaintingTask() {
