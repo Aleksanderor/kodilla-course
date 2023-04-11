bd1012d266a07a1320af28afa66c4e71a54b0d14
Aleksander Orlowski <alek.j.orlowski@gmail.com>
2023-04-11 15:59:52 +0200

    bigmac test corrected

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -2,27 +2,40 @@ package com.kodilla.patterns.builder.bigmac;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BigmacTestSuite {
 
     @Test
-    void testNewBigmac(){
+    void testNewBigMac(){
 
         //given
         Bigmac bigmac = new Bigmac.BigMacBuilder()
-                .bun("gluten Free")
+                .bun("gluten free")
                 .burgers(3)
                 .sauce("spicy")
                 .ingredient("tomato")
                 .ingredient("onions")
                 .ingredient("pickle")
                 .build();
+
         //when
         int burgerIngredients = bigmac.getIngredients().size();
+        String expectedBigMacBun = bigmac.getBun();
+        String expectedBigMacSauce = bigmac.getSauce();
+        List<String> actualBigMacIngredients = bigmac.getIngredients();
+        List<String> expectedBigMacIngredients = Arrays.asList("tomato", "onions", "pickle");
+
 
         //then
         assertEquals(3,burgerIngredients);
+        assertEquals("gluten free", expectedBigMacBun);
+        assertEquals("spicy", expectedBigMacSauce);
+        assertTrue(actualBigMacIngredients.containsAll(expectedBigMacIngredients));
 
 
     }
