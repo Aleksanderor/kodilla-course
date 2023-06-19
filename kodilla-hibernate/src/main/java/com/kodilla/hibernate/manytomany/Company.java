93a76d945ada5de4f74f14fa8f0f79d834f4dc40
Aleksander Orlowski <alek.j.orlowski@gmail.com>
2023-06-20 00:32:50 +0200

    edit

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -69,5 +69,5 @@ public class Company {
         this.employees = employees;
     }
 
-    //
+    ///
 }
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
@@ -77,3 +77,5 @@ public class Employee {
         this.companies = companies;
     }
 }
+
+//
